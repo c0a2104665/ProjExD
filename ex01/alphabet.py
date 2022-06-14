b0bf7614d8cd693c52df8329d255e84f9b19f997
Kazuki <c0a2104665@edu.teu.ac.jp>
2022-06-14 18:28:39 +0900

    何も言わないで#1

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -2,9 +2,9 @@ import string
 import random
 import time
 
-tray = 5
-all = 10
-mai = 2
+tray = 5   #実行回数
+all = 10   #アルファベットの数
+mai = 2    #欠損の数
 
 s = random.sample(string.ascii_lowercase[:all],k = all)
 
@@ -17,6 +17,8 @@ def main():
 def kaitou():
     num = int(input("欠損文字は、何文字ですか?:"))
     if num != mai:
+
+
 
 
 
