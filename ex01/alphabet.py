fe6955f380c44072a504f9dbb5e044e8b41241d6
Kazuki <c0a2104665@edu.teu.ac.jp>
2022-06-14 17:53:49 +0900

    何も言わないで

---
+++ ex01/alphabet.py
@@ -0,0 +1,55 @@
+import string
+import random
+import time
+
+tray = 5
+all = 10
+mai = 2
+
+s = random.sample(string.ascii_lowercase[:all],k = all)
+
+def main():
+    st = time.perf_counter()
+    for _ in range(tray):
+        ans = s.sample(mai)
+    en = time.perf_counter()
+
+def kaitou():
+    num = int(input("欠損文字は、何文字ですか?:"))
+    if num != mai:
+
+
+
+        
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
