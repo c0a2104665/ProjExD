a8c2aa113d9d85cc3c1795dd74d3e3d22b15e1a4
Kazuki <c0a2104665@edu.teu.ac.jp>
2022-06-21 14:36:19 +0900

    電卓

---
+++ ex02/calc.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showwarning("",f"[{num}]ボタンが押されました")
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("シンプル電卓")
+    root.geometry("300x500")
+
+
+r,c = 0,0
+for i in range(9,-1,-1):
+    button = tk.Button(root,
+                       text=f"{i}",
+                       width=4,
+                       height = 2,
+                       font = ("Times New Roman", 30)
+                    )
+    button.bind("<1>",button_click)
+    button.grid(row = r,column = c)
+    c += 1
+    if (i-1)%3 == 0:
+        r += 1
+        c = 0
+
+
+
+ 
+
+
+
+root.mainloop()    
