46c17248e990e6bca85e3c5f364b136e47bae00d
Kazuki <c0a2104665@edu.teu.ac.jp>
2022-07-05 14:27:20 +0900

    練習問題２と３

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,11 +7,27 @@ def main():
     screen_sfc = pg.display.set_mode((1600,900))
     screen_rct = screen_sfc.get_rect()
     bgimg_sfc = pg.image.load("fig/bg.png")
-    bgimg_rct = bgimg_sfc.get.rect()
+    bgimg_rct = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc,bgimg_rct)
 
     clock.tick(0.5)
 
+    kkimg_sfc = pg.image.load("fig/bg.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc,0,2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900,400
+
+
+    while True:
+        screen_sfc.blit(bgimg_sfc,bgimg_rct)
+        screen_sfc.blit(kkimg_sfc,kkimg_rct)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:return
+        
+        pg.display.update()
+        clock.tick(1000)
+        
 
 if __name__ == "__main__":
     pg.init() #pygameを初期化
